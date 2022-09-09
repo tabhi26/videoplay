c7fd2a999eef45d1cea166d92f4660e09754d8e6
abhi2138 <tiwari.abhishek@adglobal360.com>
2022-09-09 13:28:25 +0530

    project video app

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,15 @@ import Home from "./components/Home";
 import UploadVideo from "./components/UploadVideo";
 import SignUp from "./components/SignUp";
 
-
 function App() {
   return (
     <>
-    <Routes>
-      <Route exact path="/" element={<Login />}></Route>
-      <Route exact path="/signup" element={<SignUp />}></Route>
-      <Route exact path="/home" element={<Home/>}></Route>
-      <Route exact path="/uploadVideo" element={<UploadVideo/>}></Route>
-    </Routes>
+      <Routes>
+        <Route exact path="/" element={<Login />}></Route>
+        <Route exact path="/signup" element={<SignUp />}></Route>
+        <Route exact path="/home" element={<Home />}></Route>
+        <Route exact path="/uploadVideo" element={<UploadVideo />}></Route>
+      </Routes>
     </>
   );
 }
