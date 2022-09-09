7f0f1720e696a6f193eea1af639907fd03a3c4e5
abhi2138 <tiwari.abhishek@adglobal360.com>
2022-09-09 23:01:28 +0530

    home component styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,7 +7,6 @@ import UploadVideo from "./UploadVideo";
 import PlayVideo from "./PlayVideo";
 import PopUp from "./PopUp";
 import Loading from "./Loading";
-import "./Home.css"
 
 const Home = (props) => {
     const [video, setVideo] = useState([]);
@@ -34,17 +33,17 @@ const Home = (props) => {
         {props.loader ? (<Loading/>) : (
         <>
         <SideNav/>
-        <h1>Learn</h1>
+        <h1 style={{color: "black", margin: "10vh 0 0 50vh"}}>Learn</h1>
         <Button variant="contained" sx={{ backgroundColor: "crimson", marginLeft: "75%", "&:hover": {backgroundColor: 'crimson'}}} onClick={handleClick}>Upload Video</Button>
-        <div className="container">
+        <div style={{marginTop: "10px",height: "50vh", width: "120vh", marginLeft: "25%"}}>
            <BasicTable data={video} show={show} setShow={setShow}/>
         </div>
-        <div className="container">
+        <div style={{marginTop: "10px",height: "50vh", width: "120vh", marginLeft: "25%"}}>
             <PopUp title="upload" open={show} setOpen={setShow}>
                 <UploadVideo show={show} setShow={setShow} loader={props.loader} setLoader={props.setLoader} />
             </PopUp>
         </div>
-        <div className="container">
+        <div style={{marginTop: "10px",height: "50vh", width: "120vh", marginLeft: "25%"}}>
             <PopUp title="play" open={show} setOpen={setShow}>
                 <PlayVideo video={video} show={show} setShow={setShow} />
             </PopUp>
--- client/src/components/SideNav.js
+++ client/src/components/SideNav.js
@@ -1,48 +1,76 @@
-import { Button, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar } from "@mui/material";
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import {
+  Button,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+} from "@mui/material";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
 import { useNavigate } from "react-router";
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const drawerWidth = 240;
 
 const SideNav = () => {
   const navigate = useNavigate();
 
-
-
-    const drawer = (
-        <div>
-          <Toolbar />
-          <Divider />
-          <List>
-            {['UserList', 'Learn/Videos', 'Events', 'Feeds', 'Blogs'].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              </ListItem>
-            ))}
-          </List>
-        </div>
-      );
-    return (
-        <>
-        <Drawer
-          variant="permanent"
+  const drawer = (
+    <div>
+      <Toolbar />
+      <List>
+        {["UserList", "Learn/Videos", "Events", "Feeds", "Blogs"].map(
+          (text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          )
+        )}
+      </List>
+    </div>
+  );
+  return (
+    <>
+      <Drawer
+        PaperProps={{
+          sx: {
+            background: "crimson",
+          },
+        }}
+        variant="permanent"
+        sx={{
+          display: { xs: "none", sm: "block" },
+          "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+        }}
+        open
+      >
+        {drawer}
+        <Button
+          variant="contained"
           sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+            marginTop: "90%",
+            background: "white",
+            color: "crimson",
+            "&:hover": { backgroundColor: "darkGrey" },
           }}
-          open
+          onClick={() => {
+            navigate("/");
+          }}
         >
-          {drawer}
-        <Button sx={{color: "crimson"}} onClick={()=> {navigate('/')}}>Log Out</Button>
-        </Drawer>
-        </>
-    )
-}
+          <LogoutIcon />
+          Log Out
+        </Button>
+      </Drawer>
+    </>
+  );
+};
 
 export default SideNav;
