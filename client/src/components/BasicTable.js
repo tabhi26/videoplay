44cc39035afe705cd201289f55b69a14561a5945
abhi2138 <tiwari.abhishek@adglobal360.com>
2022-09-09 13:33:46 +0530

    project video app

--- client/src/components/BasicTable.js
+++ client/src/components/BasicTable.js
@@ -1,15 +1,14 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { Button } from '@mui/material';
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Button } from "@mui/material";
 
 export default function BasicTable({ data, show, setShow }) {
-
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -28,17 +27,26 @@ export default function BasicTable({ data, show, setShow }) {
           {data.map((row, index) => (
             <TableRow
               key={row._id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row">
                 {index + 1}
               </TableCell>
               <TableCell align="right">{row.title}</TableCell>
               <TableCell align="right">{row.duration}</TableCell>
-              <TableCell align="right">{new Date(row.uploadDate).toDateString()}</TableCell>
+              <TableCell align="right">
+                {new Date(row.uploadDate).toDateString()}
+              </TableCell>
               <TableCell align="right">{row.views}</TableCell>
               <TableCell align="right">{row.category}</TableCell>
-              <TableCell><Button sx={{color: "crimson"}} onClick={()=>setShow({...show, play: !show.play})}>View</Button></TableCell>
+              <TableCell>
+                <Button
+                  sx={{ color: "crimson" }}
+                  onClick={() => setShow({ ...show, play: !show.play })}
+                >
+                  View
+                </Button>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
